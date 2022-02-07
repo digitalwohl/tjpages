37b4d33859c63f68150fb3dfe037bc79dfdd49e4
Dinu Sebastian Liviu <sebastian@wearewip.com>
2022-02-07 22:13:05 +0200

    added new demo page, added sources and built page

---
+++ main.js
@@ -0,0 +1,20 @@
+import { TJ } from 'tj_comm';
+
+async function getAllWindows() {
+    const data = await TJ.chrome.windows.getAll();
+    console.log(data);
+}
+
+window.getAllWindows = getAllWindows;
+
+async function ready() {
+    const loaded = await TJ.sandbox.general.ready();
+    console.log(loaded);
+}
+
+TJ.chrome.windows.onFocusChanged((data) => {
+    console.log(data);
+})
+
+// window.addEventListener('load', ready);
+window.ready = ready;
